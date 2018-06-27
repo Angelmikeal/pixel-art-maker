0e11b89e2208fe1fa7010fc2872c4ee3b0240446
nwaokocha michael <nwaokocham@gmail.com>
2018-06-27 11:59:57 +0100

    pixel art maker first commit code

---
+++ designs.js
@@ -0,0 +1,48 @@
+$('#sizePicker').submit(function (event) {
+    event.preventDefault();
+    makeGrid();
+    colorChange();
+    colorReset();
+
+}
+);
+
+
+function makeGrid() {
+    let gridHeight = $('#inputHeight').val();
+    let gridWidth = $('#inputWidth').val();
+    let table = $('#pixelCanvas');
+    // remove existing table
+    table.children().remove();
+    //make new table row
+    for (let i = 0; i < gridHeight; i++) {
+        table.append('<tr></tr>')
+    }
+    // add cells to rows
+    for (let t = 0; t < gridWidth; t++) {
+        $('tr').append('<td></td>')
+    }
+
+}
+
+
+// fuction to change color 
+function colorChange() {
+    // this acceses the cells in the table 
+    let pixel = $('tr td');
+
+    pixel.click(function (event) {
+        var pixelColor;
+        // this declares the color
+        pixelColor = $('#colorPicker').val();
+        // this assigns the color.
+        $(this).css('background-color', pixelColor);
+    });
+
+}
+
+function colorReset() {
+    $('tr td').dblclick(function () {
+        $(this).css('background-color', '#fff');
+    })
+}
